21ab31f0806c9589c71ee5787cb2e90f03d37997
samuel mulu <samuel-mulu>
2026-01-16 10:17:49 +0300

    result(mark) full  modification

--- app/dashboard/results/continuous/[classId]/[subjectId]/[termId]/page.tsx
+++ app/dashboard/results/continuous/[classId]/[subjectId]/[termId]/page.tsx
@@ -1130,6 +1130,7 @@ export default function ContinuousResultsPage({
                   )}
                 </TableBody>
               </Table>
+              </div>
             </div>
           )}
         </CardContent>
